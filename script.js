79b0fcdcda2e1120b36cf083192b7af36a46c8e0
Birustieicom <168761745+Birustieicom@users.noreply.github.com>
2024-12-03 18:07:23 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const dino = document.querySelector(".dino");
+const dino2 = document.querySelector(".dino2");
 const cacto = document.querySelector(".cacto");
 const score = document.querySelector(".score");
 let alreadyJump = false;
@@ -13,10 +14,12 @@ document.addEventListener("keydown", (e) => {
 function jump() {
   if (!dino.classList.contains("jump")) {
     dino.classList.add("jump");
+    dino2.classList.add("jump");
     alreadyJump = true;
 
     setTimeout(() => {
       dino.classList.remove("jump");
+      dino2.classList.remove("jump");
       alreadyJump = false;
     }, 1100);
   }
@@ -31,10 +34,26 @@ setInterval(() => {
   );
 
   if (cactoLeft > 40 && cactoLeft < 270 && dinoBottom <= 50 && !alreadyJump) {
-    alert(`Game Over! Seu score foi: ${count}`);
+    alert(`Perdeu! Seu score foi: ${count}`);
     count = 0;
   }
 
   count++;
   score.innerHTML = `SCORE: ${count}`;
+
+  let dino2Bottom = parseInt(
+    window.getComputedStyle(dino2).getPropertyValue("bottom")
+  );
+  let cactoLeft = parseInt(
+    window.getComputedStyle(cacto).getPropertyValue("left")
+  );
+
+  if (cactoLeft > 40 && cactoLeft < 270 && dino2Bottom <= 50 && !alreadyJump) {
+    alert(`Perdeu! Seu score foi: ${count}`);
+    count = 0;
+  }
+
+  count++;
+  score.innerHTML = `SCORE: ${count}`;
+
 }, 10);
